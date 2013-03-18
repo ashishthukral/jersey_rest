bbd67267c8c822207e6c5d5194e1642fdef1345b
ashishthukral <yo3287@gmail.com>
2013-03-18 02:20:37 -0400

    jaxb implementation, marshall, unmarshall

--- RestProj/src/main/java/rest/JerseyClient.java
+++ RestProj/src/main/java/rest/JerseyClient.java
@@ -1,9 +1,17 @@
 package rest;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.ArrayList;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import com.google.gson.Gson;
 import com.sun.jersey.api.client.Client;
@@ -13,6 +21,13 @@ import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class JerseyClient {
+
+	public static void main(String[] args) {
+		JerseyClient theJerseyClient = new JerseyClient();
+		theJerseyClient.jerseyTest();
+		theJerseyClient.jaxbTest();
+
+	}
 
 	/*
 	 * OK 200 - The request was fulfilled.
@@ -28,9 +43,7 @@ public class JerseyClient {
 	 * 
 	 * No Response 204
 	 */
-
-	public static void main(String[] args) {
-
+	private void jerseyTest() {
 		ClientConfig config = new DefaultClientConfig();
 		Client client = Client.create(config);
 		WebResource theWebResource = client.resource(getBaseURI());
@@ -73,6 +86,63 @@ public class JerseyClient {
 	private static URI getBaseURI() {
 		// CASE-SENSITIVE URI
 		return UriBuilder.fromUri("http://localhost:8080/RestProj").build();
+	}
+
+	private void jaxbTest() {
+		ArrayList<Book> bookList = new ArrayList<Book>();
+
+		// create books
+		Book book1 = new Book();
+		book1.setIsbn("978-0060554736");
+		book1.setName("The Game");
+		book1.setAuthor("Neil Strauss");
+		book1.setPublisher("Harpercollins");
+		bookList.add(book1);
+
+		Book book2 = new Book();
+		book2.setIsbn("978-3832180577");
+		book2.setName("Feuchtgebiete");
+		book2.setAuthor("Charlotte Roche");
+		book2.setPublisher("Dumont Buchverlag");
+		bookList.add(book2);
+
+		// create bookstore, assigning book
+		Bookstore bookstore = new Bookstore();
+		bookstore.setName("Fraport Bookstore");
+		bookstore.setLocation("Frankfurt Airport");
+		bookstore.setBookList(bookList);
+
+		try {
+			// create JAXB context and instantiate marshaller
+			JAXBContext context = JAXBContext.newInstance(Bookstore.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+			// Write to System.out
+			// m.marshal(bookstore, System.out);
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			m.marshal(bookstore, baos);
+			String jaxb = baos.toString();
+			System.out.println(jaxb);
+
+			InputStream is = new ByteArrayInputStream(jaxb.getBytes());
+
+			// Write to File
+			// m.marshal(bookstore, new File(BOOKSTORE_XML));
+
+			// get variables from our xml file, created before
+			System.out.println();
+			System.out.println("Output from our XML File: ");
+			Unmarshaller um = context.createUnmarshaller();
+			Bookstore bookstore2 = (Bookstore) um.unmarshal(is);
+			ArrayList<Book> list = bookstore2.getBooksList();
+			for (Book book : list) {
+				System.out.println("Book: " + book.getName() + " from " + book.getAuthor());
+			}
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
