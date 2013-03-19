03cd05b63c3f57be57d0f3166ec8f0a765768556
ashishthukral <yo3287@gmail.com>
2013-03-19 00:58:23 -0400

    dao impl.

--- MIRProj/src/main/java/dao/UserDao.java
+++ MIRProj/src/main/java/dao/UserDao.java
@@ -20,27 +20,33 @@ public enum UserDao {
 		iUser.setId(NEXT_USER_ID);
 		_dbUserMap.put(NEXT_USER_ID, iUser);
 		NEXT_USER_ID++;
-		System.out.println(_dbUserMap);
+		LOG.info(iUser);
+		LOG.info(_dbUserMap);
 		return iUser.getId();
 	}
 
 	public User getUserById(Integer iUserId) {
 		LOG.info("getUserById");
+		LOG.info("iUserId=" + iUserId);
 		User anUser = _dbUserMap.get(iUserId);
-		System.out.println(_dbUserMap);
+		LOG.info(anUser);
+		LOG.info(_dbUserMap);
 		return anUser;
 	}
 
 	public void userUpdate(User iUser) {
 		LOG.info("userUpdate");
+		LOG.info("Old User info=" + _dbUserMap.get(iUser.getId()));
+		LOG.info("New User info=" + iUser);
 		_dbUserMap.put(iUser.getId(), iUser);
-		System.out.println(_dbUserMap);
+		LOG.info(_dbUserMap);
 	}
 
 	public void deleteUserById(Integer iUserId) {
 		LOG.info("deleteUserById");
+		LOG.info("iUserId=" + iUserId);
 		_dbUserMap.remove(iUserId);
-		System.out.println(_dbUserMap);
+		LOG.info(_dbUserMap);
 	}
 
 }
